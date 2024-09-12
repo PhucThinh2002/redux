72289210399dc36c82a9c0db6e10ee4a4b406799
thinh <duongphucthinh300702@gmail.com>
2024-09-12 23:27:57 +0700

    third

--- src/ProductManagement/ProductLayoutComponent.jsx
+++ src/ProductManagement/ProductLayoutComponent.jsx
@@ -12,6 +12,7 @@ const ProductLayoutComponent = () => {
             <li>
               <NavLink to="/" className="link my-2">Home</NavLink>
               <NavLink to="/admin" className="link">Products</NavLink>
+              <NavLink to="/admin/crypto" className="link my-2">Crypto Portfolio</NavLink>
             </li>
           </ul>
         </div>
